d775c712310c227419fb951f26f09abd211a53f4
Jarren M. Allen <jarren_a@yahoo.com>
2023-01-16 09:10:05 -0600

    Added Comments & Meaningful Commits

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,26 +7,25 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 //validattion begins here
+///checking whether dish exists
 function dishExists(req, res, next) {
-  const dishId = req.params.dishId;
-  const foundDish = dishes.find((dish) => dish.id === dishId);
+  const dishId = req.params.dishId;///extracting id
+  const found = dishes.find((dish) => dish.id === dishId); ///using .find to compare dish id to ids in data
 
-  if (foundDish) {
-    res.locals.dish = foundDish;
+  if (found) { ///if found dish is truthy store it in local memory
+    res.locals.dish = found;
     return next();
-  }
+  } ///else return error code
   next({
     status: 404,
     message: `Dish does not exist: ${dishId}`,
   });
 }
-
+///validating dish Id
 function dishIdIsValid(req, res, next) {
-  const {
-    data: { id },
-  } = req.body;
-  const dishId = req.params.dishId;
-  if (!id || dishId === id) {
+  const { data: { id }, } = req.body; 
+  const dishId = req.params.dishId; ///extracting dish id
+  if (!id || dishId === id) { ///if id matches return next else error message
     return next();
   }
   next({
@@ -34,26 +33,24 @@ function dishIdIsValid(req, res, next) {
     message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`,
   });
 }
-
+///checking for price
 function dishHasPricing(req, res, next) {
-  const {
-    data: { price },
-  } = req.body;
+  const { data: { price }, } = req.body;
 
-  if (!price) {
+  if (!price) { ///if no price return error message
     next({
       status: 400,
       message: "Dish must include a price",
     });
   }
 
-  res.locals.price = price;
+  res.locals.price = price; ///else store price in local memory
   next();
 }
-
+///validating price
 function priceIsValid(req, res, next) {
-  const price = res.locals.price;
-  if (price <= 0 || !Number.isInteger(price)) {
+  const price = res.locals.price; ///retriving price from local memory
+  if (price <= 0 || !Number.isInteger(price)) { ///if price is less than equal to zero or NaN return error code
     next({
       status: 400,
       message: `Dish must have valid price greater than 0`,
@@ -61,58 +58,53 @@ function priceIsValid(req, res, next) {
   }
   next();
 }
-
+///checking for valid image
 function dishImageIsValid(req, res, next) {
-  const {
-    data: { image_url },
-  } = req.body;
+  const { data: { image_url }, } = req.body;
 
-  if (!image_url || image_url === "") {
+  if (!image_url || image_url === "") { ///if not an image url or if empty string return erro code
     next({
       status: 400,
       message: "Dish must include a image_url",
     });
   }
 
-  res.locals.image_url = image_url;
+  res.locals.image_url = image_url; ///else store in local memory
   next();
 }
-
+///checking for a name "WE NEED A NAME" ///
 function dishIsNamed(req, res, next) {
-  const {
-    data: { name },
-  } = req.body;
+  const { data: { name }, } = req.body;
 
-  if (!name || name === "") {
+  if (!name || name === "") { ///if not name or is empty string return err mesaage
     next({
       status: 400,
       message: "Dish must include a name",
     });
   }
 
-  res.locals.name = name;
+  res.locals.name = name; ///once valid, store it in local memory
   next();
 }
-
+///checking for valind description
 function hasDescription(req, res, next) {
-  const {
-    data: { description },
-  } = req.body;
+  const { data: { description }, } = req.body;
 
-  if (!description || description === "") {
+  if (!description || description === "") { ///if not truthy description or empty string , return err 
     next({
       status: 400,
       message: "Dish must include a description",
     });
   }
 
-  res.locals.description = description;
+  res.locals.description = description; ///store in local memory
   next();
 }
 
 
-//  ROUTES
-
+///<-----ROUTES------>////
+//CRUD begns here, CREATE, READ, UPDATE, DESTROY
+//Create a dish
 function create(req, res) {
   const {
     data: { name, description, price, image_url },
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -18,12 +18,12 @@ function orderExists(req, res, next) {
 //check to see if parameter property is an Id
 function orderIdisValid(req, res, next) {
   const order = res.locals.order;
-  const { orderId } = req.params;
+  const { orderId } = req.params;///get order id from request params
   const { data: { id } = {} } = req.body;
-  if (id === "" || !id) {
+  if (id === "" || !id) { ///if id is an empty string or is not id return next
     return next();
   }
-  if (id !== orderId) {
+  if (id !== orderId) {// if id doesn not match return error message
     return next({
       status: 400,
       message: `Order id does not match route id. Order: ${id}, Route: ${orderId}`,
@@ -34,45 +34,47 @@ function orderIdisValid(req, res, next) {
 
 ///checking for address
 function hasAddress(req, res, next) {
-  const { data: { deliverTo } = {} } = req.body;
-  if (deliverTo && deliverTo !== "") {
+  const { data: { deliverTo } = {} } = req.body; 
+  if (deliverTo && deliverTo !== "") { //if address is address and not an empty string send to next
     return next();
   }
-  next({ status: 400, message: "Order must include a deliverTo" });
+  next({ status: 400, message: "Order must include a deliverTo" }); // else error message w 400 status
 }
 
 ///checking for mobile
 function hasValidMobile(req, res, next) {
   const { data: { mobileNumber } = {} } = req.body;
-  if (mobileNumber && mobileNumber !== "") {
+  if (mobileNumber && mobileNumber !== "") { ///
     return next();
   }
   next({ status: 400, message: "Order must include a mobileNumber" });
 }
 
+///function declaration to check if has dishes
 function hasDishes(req, res, next) {
-  const { data: { dishes } = [] } = req.body;
+  const { data: { dishes } = [] } = req.body; ///destructuring the request body for the dishes
   const isArray = Array.isArray(dishes);//static method determines whether the passed value is an Array then returs error
-  if (!dishes) {
-    return next({ status: 400, message: "Order must include a dish" });
-  } else if (dishes.length === 0 || !isArray) {
-    return next({
+  if (!dishes) { ///if not dishes return error
+    return next({ status: 400, message: "Order must include a dish" }); 
+  } else if (dishes.length === 0 || !isArray) { // if not res is empty or not an array of dishes return error
+    return next({ 
       status: 400,
       message: "Order must include at least one dish",
     });
   }
-  next();
+  next(); ///once validated go to next
 }
 
-function hasValidQuantity(req, res, next) {
-  const { data: { dishes } = {} } = req.body;
-  let error = null;
+//function to check quantity
+function hasValidQuantity(req, res, next) { 
+  const { data: { dishes } = {} } = req.body;///destructuring request body
+  let error = null; ///placeholder for error
   dishes.forEach((dish) => {
-    if (
+    if ( ///checking for valid number for each dish in the array
       !dish.quantity ||
       dish.quantity <= 0 ||
-      !Number.isInteger(dish.quantity)
-    ) {
+      !Number.isInteger(dish.quantity) 
+    ) { ///implicit error return
       error = {
         status: 400,
         message: `Dish ${dishes.indexOf(
@@ -86,7 +88,7 @@ function hasValidQuantity(req, res, next) {
   }
   next();
 }
-
+///checking to see if order has a status
 function hasStatus(req, res, next) {
   const { data: { status } = {} } = req.body;
   if (status && status !== "") {
@@ -98,7 +100,7 @@ function hasStatus(req, res, next) {
       "Order must have status",
   });
 }
-
+///checking if status is one of the valid stauses
 function validateStatus(req, res, next) {
   const { data: { status } = {} } = req.body;
   const statuses = [
@@ -107,11 +109,11 @@ function validateStatus(req, res, next) {
     "out-for-delivery",
     "delivered",
   ];
-
+///if statuses is not one of the valid statuses return error message 
   if (!statuses.includes(status)) {
     return next({ status: 400, message: "Invalid status" });
   }
-
+///checking if order has already been delivered
   if (status === "delivered") {
     return next({
       status: 400,
@@ -121,8 +123,9 @@ function validateStatus(req, res, next) {
 
   next();
 }
-
-//CRUD begns here
+///<-----ROUTES------>////
+//CRUD begns here, CREATE, READ, UPDATE, DESTROY
+//create an order
 function create(req, res, next) {
   const {
     data: { deliverTo, mobileNumber, status, dishes },
